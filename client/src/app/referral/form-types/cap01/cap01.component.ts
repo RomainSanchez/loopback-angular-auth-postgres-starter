8aa1c95a2c560a9347539dcd7e8bb3218499314a
Romain Sanchez <rsanchez@cdg29.bzh>
2019-12-06 16:26:15 +0100

    Update forms to comply with new regulation

--- client/src/app/referral/form-types/cap01/cap01.component.ts
+++ client/src/app/referral/form-types/cap01/cap01.component.ts
@@ -9,12 +9,6 @@ import { Component, Output, EventEmitter, Input } from '@angular/core';
 export class Cap01Component {
   @Input() data: any = {};
   @Output() formSubmit: EventEmitter<any> = new EventEmitter<any>();
-  populationChoices: any[] = [
-    '2000 à 10 000 habitants',
-    '10 000 à 20 000 habitants',
-    '20 000 à 40 000 habitants',
-    '40 000 à 80 000 habitants'
-  ];
   gradeChoices: any[] = [
     'Attaché',
     'Attaché principal',
--- server/boot/data.js
+++ server/boot/data.js
@@ -26,57 +26,52 @@ module.exports = function(app) {
   const roleMapping = [
     { principalType: 'USER', principalId: 1, roleId: 1 },
     { principalType: 'USER', principalId: 1, roleId: 2 },
-    { principalType: 'USER', principalId: 1, roleId: 3 }
+    { principalType: 'USER', principalId: 1, roleId: 3 },
+    { principalType: 'USER', principalId: 1, roleId: 4 },
+    { principalType: 'USER', principalId: 1, roleId: 5 },
+    { principalType: 'USER', principalId: 1, roleId: 6 }
   ];
 
   const forms = [
-    { name: 'Détachement sur emploi fonctionnel', committee: 'cap', code: 'cap01', requiredAttachments: ['Demande de l\'agent'] },
-    { name: 'Détachement', committee: 'cap', code: 'cap02' , requiredAttachments: ['Demande de l\'agent']},
-    { name: 'Refus de détachement de la collectivité d\'origine', committee: 'cap', code: 'cap03' , requiredAttachments: []},
-    { name: 'Mise à disposition', committee: 'cap', code: 'cap04' , requiredAttachments: ['Demande de l\'agent']},
-    { name: 'Demande d\'intégration directe', committee: 'cap', code: 'cap05' , requiredAttachments: []},
-    { name: 'Demande de changement d\'affectation dans la même collectivité', committee: 'cap', code: 'cap06' , requiredAttachments: []},
-    { name: 'Demande de disponibilité pour convenances personnelles', committee: 'cap', code: 'cap07' , requiredAttachments: ['Demande de l\'agent']},
-    { name: 'Demande de réintégration après disponibilité', committee: 'cap', code: 'cap08' , requiredAttachments: ['Demande de l\'agent']},
-    { name: 'Demande de reclassement par détachement dans un autre emploi suite à inaptitude physique', committee: 'cap', code: 'cap09' , requiredAttachments: []},
-    { name: 'Demande de licenciement pour inaptitude physique d\'un agent CNRACL', committee: 'cap', code: 'cap10' , requiredAttachments: [
+    { name: 'Refus de titularisation', committee: 'cap', code: 'cap01' , requiredAttachments: [
+      'Entretien de suivi stagiaire',
+      'Fiche de poste',
+      'Compte rendu d\'entretien',
+      'Curriculum vitae'
+    ]},
+    { name: 'Mise à disposition', committee: 'cap', code: 'cap02' , requiredAttachments: ['Demande de l\'agent']},
+    { name: 'Demande de reclassement par détachement dans un autre emploi suite à inaptitude physique', committee: 'cap', code: 'cap03' , requiredAttachments: []},
+    { name: 'Demande de licenciement pour inaptitude physique d\'un agent CNRACL', committee: 'cap', code: 'cap04' , requiredAttachments: [
       'PV du comité médical',
       'Etat récapitulatif des arrêts',
       'Refus CNRACL',
       'Fiche d\'aptitude du médecin de prévention',
       'Impossibilité de reclassement'
     ]},
-    { name: 'Demande de licenciement pour inaptitude physique d\'un agent IRCANTEC', committee: 'cap', code: 'cap11' , requiredAttachments: [
+    { name: 'Demande de licenciement pour inaptitude physique d\'un agent IRCANTEC', committee: 'cap', code: 'cap05' , requiredAttachments: [
       'PV du comité médical',
       'Etat récapitulatif des arrêts',
       'Fiche d\'aptitude du médecin de prévention',
       'Impossibilité de reclassement'
     ]},
-    { name: 'Prorogation de stage', committee: 'cap', code: 'cap12' , requiredAttachments: [
+    { name: 'Prorogation de stage', committee: 'cap', code: 'cap06' , requiredAttachments: [
       'Entretien de suivi stagiaire',
       'Fiche de poste',
       'Compte rendu d\'entretien',
       'Curriculum vitae'
     ]},
-    { name: 'Refus de titularisation', committee: 'cap', code: 'cap13' , requiredAttachments: [
+    { name: 'Refus de détachement de la collectivité d\'origine', committee: 'cap', code: 'cap07' , requiredAttachments: []},
+    { name: 'Licenciement en cours de stage', committee: 'cap', code: 'cap08' , requiredAttachments:[
       'Entretien de suivi stagiaire',
       'Fiche de poste',
       'Compte rendu d\'entretien',
       'Curriculum vitae'
     ]},
-    { name: 'Licenciement en cours de stage', committee: 'cap', code: 'cap14' , requiredAttachments:[
-      'Entretien de suivi stagiaire',
-      'Fiche de poste',
-      'Compte rendu d\'entretien',
-      'Curriculum vitae'
-    ]},
-    { name: 'Proposition d\'avancement à l\'échelon spécial', committee: 'cap', code: 'cap15' , requiredAttachments: []},
-    { name: 'Proposition d\'avancement de grade', committee: 'cap', code: 'cap16' , requiredAttachments: []},
-    { name: 'Demande de révision de l\'entretien professionnel', committee: 'cap', code: 'cap17' , requiredAttachments: ['Compte rendu d\'entretien']},
-    { name: 'Promotion interne', committee: 'cap', code: 'cap18' , requiredAttachments: []},
-    { name: 'Refus de congé de formation syndicale ou professionnelle', committee: 'cap', code: 'cap19' , requiredAttachments: []},
-    { name: 'Suppression d\'emploi', committee: 'cap', code: 'cap20' , requiredAttachments: []},
-    { name: 'Transfert de personnel', committee: 'cap', code: 'cap21' , requiredAttachments: []},
+    { name: 'Proposition d\'avancement à l\'échelon spécial', committee: 'cap', code: 'cap09' , requiredAttachments: []},
+    { name: 'Proposition d\'avancement de grade', committee: 'cap', code: 'cap10' , requiredAttachments: []},
+    { name: 'Demande de révision de l\'entretien professionnel', committee: 'cap', code: 'cap11' , requiredAttachments: ['Compte rendu d\'entretien']},
+    { name: 'Promotion interne', committee: 'cap', code: 'cap12' , requiredAttachments: []},
+    { name: 'Refus de congé de formation syndicale ou professionnelle', committee: 'cap', code: 'cap13' , requiredAttachments: []},
     { name: 'Règlement intérieur', committee: 'ct' , code: 'ct01', requiredAttachments: ['Projet de règlement intérieur']},
     { name: 'Mise en place du temps partiel sur autorisation', committee: 'ct' , code: 'ct02', requiredAttachments: []},
     { name: 'Formation', committee: 'ct' , code: 'ct03', requiredAttachments: [
